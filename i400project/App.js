4a7c8d4708f70e045873762d9b77dbcf2dbbb0c4
kayzhangg <142861113+kayzhangg@users.noreply.github.com>
2023-10-17 23:36:34 -0400

    milestone1, add task component

--- i400project/App.js
+++ i400project/App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {Task} from './Task.js';
+import { StyleSheet, View, Text } from 'react-native';
+import Task from './Task.js';
+import { useState } from 'react';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Personal</Text>
-      <View>
-        <Task text="Text1"/>
-        <Task text="Text2"/>
+      <View style={styles.titlecontainer}>
+        <Text style={styles.sectionTitle}>Personal</Text>
+      </View>
+      <View style={styles.taskcontainer}>
+        <Task text="task1" priority="red"/>
+        <Task text="task2" priority="yellow"/>
       </View>
     </View>
   );
@@ -18,7 +21,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  titlecontainer:{
+    backgroundColor: "lavender",
+    padding: 50,
+    borderRadius:30,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  taskcontainer:{
+    marginTop:20
+  }
 });
